fd3069a7c8e1387512999ea39c44a6ef2ef46761
wll8 <xwdaysme@gmail.com>
2019-12-29 12:37:31 +0800

    优化前端代码打包体积
    ## 优化前
    
    ``` txt
      File                                      Size             Gzipped
    
      ..\web-ui\js\chunk-vendors.76fde676.js    854.55 KiB       241.52 KiB
      ..\web-ui\js\app.5f35c939.js              7.67 KiB         3.14 KiB
      ..\web-ui\css\chunk-vendors.c93adbda.c    205.39 KiB       33.79 KiB
      ss
      ..\web-ui\css\app.d24267bd.css            0.28 KiB         0.19 KiB
    ```
    
    ## 优化后
    ``` txt
        File                                      Size             Gzipped
    
      ..\web-ui\js\chunk-vendors.3a3a4c5a.js    471.69 KiB       145.39 KiB
      ..\web-ui\js\app.391acb21.js              8.22 KiB         3.35 KiB
      ..\web-ui\css\chunk-vendors.4fce83f0.c    59.71 KiB        10.75 KiB
      ss
      ..\web-ui\css\app.d24267bd.css            0.28 KiB         0.19 KiB
    ```

--- web-ui-src/src/plugins/element.js
+++ web-ui-src/src/plugins/element.js
@@ -1,4 +1,26 @@
 import Vue from 'vue';
-import Element from 'element-ui';
+import {
+  Dialog,
+  Form,
+  FormItem,
+  Aside,
+  Container,
+  Tooltip,
+  Switch,
+  Menu,
+  MenuItem,
+  Button,
+  Select
+} from 'element-ui';
 
-Vue.use(Element);
+Vue.use(Dialog);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Aside);
+Vue.use(Container);
+Vue.use(Tooltip);
+Vue.use(Switch);
+Vue.use(MenuItem);
+Vue.use(Menu);
+Vue.use(Button);
+Vue.use(Select);
